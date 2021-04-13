e20916e4ed54c7079d68a6223cad39093d9fbd3b
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-04-14 04:03:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 pkg update
 pkg upgrade
+pkg install toilet
 pkg install git 
 pkg install nodejs
 pkg install libwebp
@@ -11,4 +12,5 @@ pkg install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install -g 
 clear
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+echo "[*] All dependencies have been installed, please run the command \"bash run.sh\" to immediately start the script"
+echo "[*] Ketik $ bash run.sh Untuk Menjalankan"
