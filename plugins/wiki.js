d440a2fda99ac9fdfa15b35323df962cd9d54fea
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-05-08 05:23:05 +0700

    update wiki.js

--- plugins/wiki.js
+++ plugins/wiki.js
@@ -6,7 +6,9 @@ const res = await axios.get(`https://id.m.wikipedia.org/w/index.php?search=${que
 const $ = cheerio.load(res.data)
 const hasil = []
 let wiki = $('#mf-section-0').find('p').text()
-let thumb = 'https:' + $('#mf-section-0').find('div > div > a > img').attr('src')
+let thumb = $('#mf-section-0').find('div > div > a > img').attr('src')
+thumb = thumb ? thumb : '//pngimg.com/uploads/wikipedia/wikipedia_PNG35.png'
+thumb = 'https:' + thumb
 let judul = $('h1#section_0').text()
 hasil.push({ wiki, thumb, judul })
 return hasil
