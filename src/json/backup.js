30ab27b999434871a00acad428f79121bd69a667
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-08-09 00:15:11 +0700

    Ngehehe

---
+++ src/json/backup.js
@@ -0,0 +1,29 @@
+{
+  "botname": "Zbin-Wabot",
+  "ownerNumber": ["6281910263857","6283128671683"],
+  "prefix": ["z"],
+  "ownername": "Galang/ZbinKw",
+  "unicode": {
+    "list": "+",
+    "head": "#",
+    "body": "-",
+    "wings": [">","<"],
+    "end": "•"
+  },
+  "response":{
+    "owner": "BIKIN SENDIRI MSG NYA ASW",
+    "admin": "BIKIN SENDIRI MSG NYA ASW",
+    "mods": "BIKIN SENDIRI MSG NYA ASW",
+    "premium": "BIKIN SENDIRI MSG NYA ASW",
+    "private": "BIKIN SENDIRI MSG NYA ASW",
+    "group": "BIKIN SENDIRI MSG NYA ASW",
+    "botAdmin": "BIKIN SENDIRI MSG NYA ASW"
+  },
+  "session":{
+    "clientID": "B+b4q7FZ4ZR5FTk6CApKzA==",
+    "serverToken": "1@aZNfuo95cTCKtXBPbQmbxmYrrEB7NFtQ6nTKODZ82P4GQ0qcaVUiAzNcxvBy69KZ6Pg1I/8sSYAi9A==",
+    "clientToken": "J6MqlfIV1hT/0VKl5eUyyge3BWTJxH7VvcieKpKBcgk=",
+    "encKey": "ULoiAL0VXD9cpyJCKJ12u5Ij+Cq1YZKAxNgM1Ntv36Y=",
+    "macKey": "IcPN0+qx34fehaucGOvMlP47VMrLPOF9m4fbU4psRlA="
+  }
+}
