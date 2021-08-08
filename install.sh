863e58cc3d69f686f50794b264f8135cd2cbfffd
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-08-09 00:10:38 +0700

    Update Bang

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/bash
 
-pkg update
-pkg upgrade
-pkg install toilet
-pkg install git 
-pkg install nodejs
-pkg install libwebp
-pkg install ffmpeg
-pkg install wget
-pkg install tesseract
+apt update && apt upgrade
+apt install git libwebp ffmpeg tesseract nodejs
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install -g 
-clear
-echo "[*] All dependencies have been installed, please run the command \"bash run.sh\" to immediately start the script"
-echo "[*] Ketik $ bash run.sh Untuk Menjalankan"
+Instalation Done, node index atau npm start untuk run bot
