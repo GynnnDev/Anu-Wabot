2325f9790db9427dcfadbdd5267bfd3f9553c22e
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-08-14 23:37:45 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
-
-apt update && apt upgrade
-apt install git libwebp ffmpeg tesseract nodejs
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+apt-get update && apt-get upgrade
+apt-get install git libwebp ffmpeg nodejs
 npm install -g 
-Instalation Done, node index atau npm start untuk run bot
+echo "Instalation Complete, Donate Lah Bang:v"
