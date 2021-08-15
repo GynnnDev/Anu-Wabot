54ff12b238e6a99a4e4a369df749a3aecf32829e
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-08-15 15:09:55 +0700

    update ownerCmd.js

--- command/ownerCmd.js
+++ command/ownerCmd.js
@@ -1,1 +1,22 @@
-//Owner Command, Just Basic Command Inside It
+cmd.on('eval',['>','=>'],['owner'],async(msg,{command,text}) => {
+
+let parse = command.includes('=>') ? text.replace('=>','return ') : text.replace('>','')
+
+try{
+let evaluate = await eval(`;(async () => {${parse} })()`).catch(e => { return e });
+return client.reply(msg,functions.util.format(evaluate));
+} catch(e){
+return client.reply(msg,functions.util.format(e));
+}
+},{owner:true,usedPrefix:false})
+
+cmd.on('exec',['\\$'],['owner'],async(msg,{query,commandNpref}) => {
+try{
+functions.exec(`${query}`,(err,out) => {
+if (err) return client.reply(msg,functions.util.format(err));
+client.reply(msg,functions.util.format(out));
+});
+} catch(e){
+ return client.reply(msg,functions.util.format(e));
+}
+},{owner:true,usedPrefix:false})
