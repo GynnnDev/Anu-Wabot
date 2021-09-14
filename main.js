9b75b0674d827c38104a58e697931698ada86e3f
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-09-14 19:02:39 +0700

    fix

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ const { WAConnection, Functions } = require('./lib/functions.js');
 const Command = require('./lib/command.js');
 
 global.baileys = require('@adiwajshing/baileys');
-global.media = require('./src/json/media.json');
 global.botinfo = require('./src/json/botInfo.json');
 global.userDb = require('./src/json/user.json');
 global.groupDb = require('./src/json/group.json');
 global.functions = new Functions();
 global.client = new WAConnection();
 global.cmd = new Command(client, global.botinfo, global.functions);
-global.logo = { buffer:functions.fs.readFileSync('./src/images/logo.jpg'),message:media.logo };
-global.clients = {};
+global.logo = {buffer:functions.fs.readFileSync('./src/images/logo.jpg')};
+global.clients = [];
 
 async function run(){
 await functions.start();
-for (let a of functions.fs.readdirSync('./lib/command')) require(`./lib/command/${a}`);
+for (let a of functions.fs.readdirSync('./lib/command')) require(`./lib/command/${a}`)
 for (let b of functions.fs.readdirSync('./lib/actions')) require(`./lib/actions/${b}`);
 await functions.delay(1000);
 functions.animate.succeed('Loading',{text:'Checking And Adding New Command Succeed'});
@@ -23,5 +22,6 @@ client.logger.level = 'error';
 client.browserDescription = ['Zbin-Wabot','Desktop','3.0'];
 botinfo.session && await client.loadAuthInfo(botinfo.session);
 await client.connect({timeoutMs: 30000});
+logo.message = (await client.prepareMessageMedia(logo.buffer,'imageMessage')).imageMessage
 }
 run();
