c0a6e34df6310b99d75b5bab21d830cc7ec48899
Zyy <50652852+Azyansah@users.noreply.github.com>
2021-11-27 10:52:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const { exec,execSync,spawn } = require('child_process');
+const {
+  exec,
+  execSync,
+  spawn
+} = require('child_process');
 const Functions = require('./lib/functions.js');
 const Command = require('./lib/command.js');
 const WAConnection = require('./lib/waconnection.js');
 
-global.baileys = require('@adiwajshing/baileys');
+global.baileys = require('@adiwajshing/baileys-md');
 global.botinfo = require('./src/json/botInfo.json');
 global.functions = new Functions();
 global.client = new WAConnection();
 global.cmd = new Command(client, botinfo, functions);
-global.logo = []
 global.clients = [];
-global.used_logo = 0
 
 functions.run(client);
